9f2c47b3daebee5604d9a87126f6ae56cfb9f194
gaotongfont <150329900+gaotongfont@users.noreply.github.com>
2024-07-30 14:46:22 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-# GT_HMI_Engine_arduino
-GT-HMI-Engine as Arduino Library + example
+<br/>
+
+<div align="center">
+    <img src="extras/img/logo.png">
+</div>
+
+<br/>
+<div align="center">  <a><b>GT-HMI-Engine arduino 库</b></a></div>
+<div align="center">  <a><b>致力于低代码开发，多功能，易用的免费嵌入式图形库</b></a></div>
+
+---
+
+<br/>
+<div align="center"
+>
+    <a href="https://www.hmi.gaotongfont.cn/kfgj"
+    > 公司官网 </a> |
+    <a href="https://space.bilibili.com/3493293474188211/video"
+    > B 站教程 </a> |
+    <a href="https://www.hmi.gaotongfont.cn/kfgj#/#hmiEngine"
+    > Engine 使用手册 </a> |
+    <a href="https://www.hmi.gaotongfont.cn/kfgj#/#hmidesigner"
+    > Designer 使用手册 </a> |
+    <a href="http://isite.baidu.com/site/wjz7qkrv/406a2b0c-f9c7-4a08-a47a-662e862b2af4?ch=48&wid=498ccd5c05334f21a2142ba3cf628964_0_0&field=&orderBy=&categoryId=undefined&title=%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC"
+    > 联系方式 </a> |
+    <a href="https://genitop-1317577547.cos.ap-nanjing.myqcloud.com/GT-HMI/GT-HMI-Groups/GT-HMI%20Communication%20groups.jpg"
+    > GT HMI微信交流群 </a>
+</div>
+<br/>
+
+#### 介绍
+- **GT-HMI-Engine arduino库目录结构**
+<table>
+<tr>
+  <th style="width:40%">目录</th>
+  <th style="width:60%">说明</th>
+</tr>
+<tr>
+  <td>examples/</td>
+  <td>存放库的示例程序</td>
+</tr>
+<tr>
+  <td>extras/</td>
+  <td>一些辅助性的文件，不是库的核心组成部分</td>
+</tr>
+<tr>
+  <td>src/</td>
+  <td>库的核心部分，GT-HMI-Engine源码文件</td>
+</tr>
+<tr>
+  <td>keywords.txt</td>
+  <td>关键字文件</td>
+</tr>
+<tr>
+  <td>library.properties</td>
+  <td>库特性说明文件</td>
+</tr>
+</table>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;该库采用GT-HMI-Engine(v1.3.x)，用做arduino库，库添加可以通过.zip库导入或者直接把整个文件夹放到库路径下。
+
+#### 示例
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在examples目录下有一个简单的示例ESP32S3_TFT2_4.INO，展示了如何使用GT-HMI-Engine实现控件的显示以及控件事件的触发。
+
+**1. 开发环境**：
+   - IDE：VSCode + PlatformIO / Arduino IDE
+
+**2. 硬件**：
+   - 主控设备：ESP32S3开发板;
+   - 显示设备：2.4寸TFTLCD ST7789V 320*240 (无触摸);
+   - 输入设备：物理按键3个，按键PREV和NEXT用于切换控件焦点，按键ENTER用于确认焦点，相当触摸的点击作用，触发事件实现界面的交互。
+
+**3.修改以下文件配置**：
+
+- **gt_conf.h**
+把GT_SCREEN_WIDTH改成320，GT_SCREEN_HEIGHT改成240。
+![alt text](extras/img/image-5.png)
+
+- **gt_port_disp.c**
+首先，把头文件"esp_heap_caps.h"添加进来，
+![alt text](extras/img/image-11.png)
+然后，把buff_all数组使用PSRAM进行内存分配。
+![alt text](extras/img/image-12.png)
+最后，还需要修改platformio.ini文件，在这里指定FLASH和PSRAM的运行模式以及启动PSRAM。
+![alt text](extras/img/image-19.png)
+
+- **gt_port_indev.c**
+该示例使用了3个物理按键，需要在gt_port_indev_init()里，把注册输入设备按键的类型改成GT_INDEV_TYPE_KEYPAD。
+![alt text](extras/img/image-18.png)
+
+- **gt_port_disp.c，gt_port_indev.c，gt_port_vf.c**
+把这几个.c文件都改成.cpp文件，如下图所示：
+![alt text](extras/img/image-14.png)
+
+编译无报错，下载到开发板，示例效果如下所示：
+![](extras/img/example.jpg)
+
+
+
+
+
+
+
+
+
+
---
+++ examples/ESP32S3_TFT2_4/ESP32S3_TFT2_4.ino
@@ -0,0 +1,168 @@
+#include <Arduino.h>
+#include "uart.h"
+#include "xl9555.h"
+#include "spilcd.h"
+#include "tim.h"
+#include "gt.h"
+
+/**
+ * @brief 刷屏函数
+ *
+ * @param drv   不需要配置
+ * @param area  area->x绘制区域的起始x坐标
+ *              area->y绘制区域的起始y坐标
+ *              area->w绘制区域的宽度
+ *              area->h绘制区域的高度
+ * @param color
+ */
+void _flush_cb(struct _gt_disp_drv_s * drv, gt_area_st * area, gt_color_t * color)
+{
+    uint32_t start_x = area->x, start_y = area->y;
+    uint32_t end_x = start_x + area->w, end_y = start_y + area->h;
+    //遍历绘制区域内的每个像素
+    for (uint32_t y = start_y; y < end_y; y++) {
+        for (uint32_t x = start_x; x < end_x; x++) {
+            //画点
+            lcd_draw_point(x, y, color->full);
+            color++;
+        }
+    }
+
+}
+
+
+/**
+ * @brief 触摸函数
+ *
+ * @param indev_drv 不需要配置
+ * @param data      data->state 触摸状态
+ *                  data->point.x 触摸点x坐标
+ *                  data->point.y 触摸点y坐标
+ */
+void read_cb(struct _gt_indev_drv_s * indev_drv, gt_indev_data_st * data)
+{
+    // if (!tp_dev.scan(1)) {
+    //     data->state = GT_INDEV_STATE_RELEASED;
+    //     return;
+    // }
+    // data->point.x = tp_dev.x[0];
+    // data->point.y = tp_dev.y[0];
+    // data->state = GT_INDEV_STATE_PRESSED;
+}
+
+/**
+ * @brief 按键函数
+ *
+ * @param indev_drv 不需要配置
+ * @param data      data->state 按键状态
+ *                  data->key   按键功能
+ */
+void read_cb_btn(struct _gt_indev_drv_s * indev_drv, gt_indev_data_st * data)
+{
+    uint8_t status = 0;
+    status = xl9555_key_scan(1);
+    static int remark = 0;
+    switch (status) {
+    case KEY0_PRES:
+        data->state = GT_INDEV_STATE_PRESSED;
+        data->key = GT_KEY_ENTER;
+        break;
+    case KEY1_PRES:
+        data->state = GT_INDEV_STATE_PRESSED;
+        data->key = GT_KEY_NEXT;
+        break;
+    case KEY2_PRES:
+        data->state = GT_INDEV_STATE_PRESSED;
+        data->key = GT_KEY_PREV;
+        break;
+    default:
+        if (remark) {
+            data->state = GT_INDEV_STATE_RELEASED;
+        }
+        else {
+            data->state = GT_INDEV_STATE_INVALID;
+        }
+        data->key = GT_KEY_NONE;
+        remark = 0;
+        break;
+    }
+    remark = status;
+}
+
+/**
+ * @brief spi读写
+ *
+ * @param data_write    写入数据
+ * @param len_write     写入数据长度
+ * @param data_read     读取数据
+ * @param len_read      读取数据长度
+ * @return uint32_t
+ */
+uint32_t spi_wr(uint8_t * data_write, uint32_t len_write, uint8_t * data_read, uint32_t len_read)
+{
+    // uint32_t i;
+    // digitalWrite(FLASH_CS, LOW);
+    // for (i = 0; i < len_write; i++) {
+    //     SPI.transfer(data_write[i]);
+    // }
+    // for (i = 0; i < len_read; i++) {
+    //     data_read[i] = SPI.transfer(0xFF);
+    // }
+    // digitalWrite(FLASH_CS, HIGH);
+    return 0;
+}
+
+static void btn_click_cb(gt_event_st * e)
+{
+    static int cnt_idx = 0;
+    char buffer[100];
+
+    memset(buffer, 0, sizeof(buffer));
+    sprintf(buffer, "btn %d", ++cnt_idx);
+    printf("%s\n", buffer);
+    gt_btn_set_text(e->target, buffer);
+}
+
+
+/* global functions / API interface -------------------------------------*/
+
+/**
+ * @brief create a button, and click it will printf touch count.
+ */
+void gt_examples_button(void)
+{
+    gt_obj_st * screen;
+    screen = gt_obj_create(NULL);
+
+    gt_obj_st * btn = gt_btn_create(screen);
+    gt_obj_set_pos(btn, 0, 30);
+    gt_obj_set_size(btn, 80, 40);
+    gt_obj_add_event_cb(btn, btn_click_cb, GT_EVENT_TYPE_INPUT_RELEASED, NULL);
+
+    gt_obj_st * btn1 = gt_btn_create(screen);
+    gt_obj_set_pos(btn1, 100, 30);
+    gt_obj_set_size(btn1, 80, 40);
+    gt_obj_add_event_cb(btn1, btn_click_cb, GT_EVENT_TYPE_INPUT_RELEASED, NULL);
+
+    gt_obj_st * btn2 = gt_btn_create(screen);
+    gt_obj_set_pos(btn2, 200, 30);
+    gt_obj_set_size(btn2, 80, 40);
+    gt_obj_add_event_cb(btn2, btn_click_cb, GT_EVENT_TYPE_INPUT_RELEASED, NULL);
+
+    gt_disp_load_scr(screen);
+}
+
+void setup() {
+    uart_init(0, 115200);       /* 串口0初始化 */
+    xl9555_init();              /* IO扩展芯片初始化 */
+    lcd_init();                 /* LCD初始化 */
+    timx_int_init(10, 8000);    /* 定时器初始化,定时时间为1ms */
+    lcd_clear(WHITE);
+    gt_init();
+    gt_examples_button();
+}
+
+void loop() {
+    gt_task_handler();          /* gui的事务处理函数 */
+    delay(1);
+}
